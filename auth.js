c58972686935d1eb5a4ab5d1e95e63ae61074d7f
Jadiscke <vjtasso@gmail.com>
2025-01-13 11:06:45 -0300

    chore remove prisma

--- auth.js
+++ auth.js
@@ -1,9 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
   providers: [GithubProvider],
 });
